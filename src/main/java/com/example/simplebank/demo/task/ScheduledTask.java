09aa28645eb547939daade13a05653251a75be85
Nikola Brodar <brodarnikola@gmail.com>
2024-08-08 12:22:47 +0200

    Return back execution of task to 2 hours, for calculating previous month turnover

--- src/main/java/com/example/simplebank/demo/task/ScheduledTask.java
+++ src/main/java/com/example/simplebank/demo/task/ScheduledTask.java
@@ -21,7 +21,7 @@ public class ScheduledTask {
     // month
     // day of week
     // execute every 2 hours
-    @Scheduled(cron = "*/10 * * * * *")
+    @Scheduled(cron = "0 0 */2 * * *")
     public void task1() {
         accountService.getAllOriginalAccounts().forEach(account -> accountService.getLastMonthTurnOver(account, transactionService));
     }
