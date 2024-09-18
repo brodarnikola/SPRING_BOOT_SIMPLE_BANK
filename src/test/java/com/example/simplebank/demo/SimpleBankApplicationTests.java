7e57b43c9899f6e83f2fdb6b7c7fe80f87f09b84
Nikola Brodar <brodarnikola@gmail.com>
2024-09-18 11:23:43 +0200

    Comment test for running sprinboot application

--- src/test/java/com/example/simplebank/demo/SimpleBankApplicationTests.java
+++ src/test/java/com/example/simplebank/demo/SimpleBankApplicationTests.java
@@ -3,11 +3,11 @@ package com.example.simplebank.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class SimpleBankApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class SimpleBankApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
